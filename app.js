548ffdf45afebdb3064373530eb0ca435759c619
Pospíchal Šimon <L20220048@SSPTAJI.CZ>
2024-10-14 12:28:23 +0200

    xd

---
+++ app.js
@@ -0,0 +1,45 @@
+function vypocetohmovazakona() {
+
+    let U = parseFloat(document.getElementById('napeti').value);
+
+    let I = parseFloat(document.getElementById('proud').value);
+
+    let R = parseFloat(document.getElementById('odpor').value);
+
+    let result = "";
+
+
+    if (!isNaN(U) && !isNaN(I)) {
+
+        R = U / I;
+
+        result = "Odpor (R) = " + R.toFixed(2) + " Ω";
+
+        document.getElementById('odpor').value = R.toFixed(2);
+
+    } else if (!isNaN(U) && !isNaN(R)) {
+
+        I = U / R;
+
+        result = "Proud (I) = " + I.toFixed(2) + " A";
+
+        document.getElementById('proud').value = I.toFixed(2);
+
+    } else if (!isNaN(I) && !isNaN(R)) {
+
+        U = I * R;
+
+        result = "Napětí (U) = " + U.toFixed(2) + " V";
+
+        document.getElementById('napeti').value = U.toFixed(2);
+
+    } else {
+
+        result = "Prosím zadejte dvě hodnoty pro výpočet.";
+
+    }
+
+
+    document.getElementById('result').innerHTML = result;
+
+}
