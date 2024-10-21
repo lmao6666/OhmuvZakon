09d805f906c73490866b075a2ffc17daf8189876
Pospíchal Šimon <L20220048@SSPTAJI.CZ>
2024-10-21 11:54:51 +0200

    service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-// This code executes in its own worker or thread
-self.addEventListener("install", event => {
-    console.log("Service worker installed");
- });
- self.addEventListener("activate", event => {
-    console.log("Service worker activated");
- });
+self.addEventListener('install', event => {
+   console.log('Service Worker se instaluje...');
+   event.waitUntil(
+   caches.open('my-cache').then(cache => {
+   return cache.addAll([
+   '/',
+   '/index.html',
+   '/style.css',
+   '/app.js',
+   '/manifest.json',
+   '/cannabis.webp',
+   '/diktator1.png',
+   '/diktator2.png',
+   ]);
+   })
+   );
+   });
+   // Aktivace service workeru
+   self.addEventListener('activate', event => {
+   console.log('Service Worker byl aktivován');
+   });
